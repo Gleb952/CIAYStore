29aca724108fd2baa882479dd90a6928f3d1540b
Gleb952 <lomakin.plex@mail.ru>
2022-07-28 14:13:29 +0300

    goodsOut container posters

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -1,17 +1,29 @@
 function init() {
     // читаем файл goods.json
-    $.getJSON("good.json", goodsOut);
+    $.getJSON('good.json', goodsOut);
 }
 
 function goodsOut(data) {
-    // вывод товара на страницу
-    /* var goods = JSON.parse(data); */
+    // вывод на страницу
     console.log(data);
+    var out = '';
+    for (var key in data) {
+        out += '<div class="card col">';
+        out += '<img clas="card_img_top img_goods" src="img/' + data[key].img_goods + '" alt="photo">';
+        out += '<div class="card-body">';
+        out += '<h5 class="card-title text-center name">' + data[key].name + '</h5>';
+        out += '<p class="card-text text-secondary text-center cost">' + data[key].cost + '</p>';
+        out += '</div>';
+        out += '<button type="button" class="btn btn-outline-secondary">By</button>';
+        out += '</div>';
 
+
+    }
+    $('.goods_out').html(out);
 }
+
 
 
 $(document).ready(function () {
     init();
-
 });
