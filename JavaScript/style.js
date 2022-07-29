445d78c4618568abce42294c3b355eed3af0dfd5
Gleb952 <lomakin.plex@mail.ru>
2022-07-29 22:00:15 +0300

    function BankCardSticker posters, function addToBacket

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -1,9 +1,9 @@
 var cart = {}; //корзина backet
-function init() {
+function posters() {
     // читаем файл goods.json
-    $.getJSON('good.json', goodsOut);
+    $.getJSON('posters.json', postersOut);
 
-    function goodsOut(data) {
+    function postersOut(data) {
         // вывод на страницу
         console.log(data);
         var out = '';
@@ -19,24 +19,55 @@ function init() {
 
 
         }
-        $('.goods_out').html(out);
-        $('.addToBacket').on('click', addToBacket);
+        $('.goods_outPosters').html(out);
+
     }
-    function addToBacket (){
+}
+
+function BankCardSticker() {
+    // читаем файл BankCardSticker.json
+    $.getJSON('BankCardSticker.json', BankCardStickerOut);
+
+    function BankCardStickerOut(data) {
+        // вывод на страницу
+        console.log(data);
+        var out = '';
+        for (var key in data) {
+            out += '<div class="card col">';
+            out += '<img clas="card_img_top img_goods" src="img/' + data[key].img_goods + '" alt="photo">';
+            out += '<div class="card-body">';
+            out += '<h5 class="card-title text-center name">' + data[key].name + '</h5>';
+            out += '<p class="card-text text-secondary text-center cost">' + data[key].cost + '</p>';
+            out += '</div>';
+            out += `<button type="button" class="btn btn-outline-secondary addToBacket" data-id="${key}">By</button>`;
+            out += '</div>';
+
+
+        }
+        $('.goods_outBankCardSticker').html(out);
+
+        /* <---------------------------NO COPY---------------------------> */
+        $('.addToBacket').on('click', addToBacket); // One universal event :)
+        // should be the last !!!
+        /* <---------------------------NO COPY---------------------------> */
+    }
+
+    function addToBacket() {
         var id = $(this).attr('data-id');
         console.log(id);
 
 
-        if (cart[id]==undefined){
+        if (cart[id] == undefined) {
             cart[id] = 1;
-        }
-        else {
+        } else {
             cart[id]++;
         }
         console.log(cart);
     }
 
 }
+
 $(document).ready(function () {
-    init();
+    posters();
+    BankCardSticker();
 });
