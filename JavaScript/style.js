37c19fb91329ca2089cca451debd9a85a79e46fd
Gleb952 <lomakin.plex@mail.ru>
2022-07-29 22:29:48 +0300

    BankCardSticker  posters check Sticker

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -23,7 +23,30 @@ function posters() {
 
     }
 }
+function Sticker() {
+    // читаем файл goods.json
+    $.getJSON('Stickers.json', StickersOut);
 
+    function StickersOut(data) {
+        // вывод на страницу
+        console.log(data);
+        var out = '';
+        for (var key in data) {
+            out += '<div class="card col">';
+            out += '<img clas="card_img_top img_goods" src="img/' + data[key].img_goods + '" alt="photo">';
+            out += '<div class="card-body">';
+            out += '<h5 class="card-title text-center name">' + data[key].name + '</h5>';
+            out += '<p class="card-text text-secondary text-center cost">' + data[key].cost + '</p>';
+            out += '</div>';
+            out += `<button type="button" class="btn btn-outline-secondary addToBacket" data-id="${key}">By</button>`;
+            out += '</div>';
+
+
+        }
+        $('.goods_outBankCardSticker').html(out);
+
+    }
+}
 function BankCardSticker() {
     // читаем файл BankCardSticker.json
     $.getJSON('BankCardSticker.json', BankCardStickerOut);
@@ -69,5 +92,7 @@ function BankCardSticker() {
 
 $(document).ready(function () {
     posters();
+    Sticker();
     BankCardSticker();
+
 });
