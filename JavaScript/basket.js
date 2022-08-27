b0b744f3bc3f1ca6977d94023fe38eb3fa6bb0bd
Gleb952 <lomakin.plex@mail.ru>
2022-08-27 11:21:29 +0300

    first basket

--- JavaScript/basket.js
+++ JavaScript/basket.js
@@ -3,25 +3,36 @@ var cart = {};
 function loadCart() {
     if (localStorage.getItem('cart')) {
         cart = JSON.parse(localStorage.getItem('cart'));
-        $(".cart").html("None");
         showCart();
+    } else {
+        $(".cart").html("None");
     }
 }
 
-function showCart() {
-    $.getJSON(("posters.json", "Stickers.json", "Keychains.json", "Belts.json", "Lanyard.json", "BankCardSticker.json"), function (data) {
-        var goods = {};
-        var out = "";
-        for (var key in cart) {
-            out += ``
-        }
 
-    });
-}
+
+
+function showCart() {
+    $.getJSON("posters.json", function (data) {
+        var goods = data;
+        var out = '';
+        for (var id in cart) {
+            out += `<div class="cartBasket">`;
+            out += `<img class="imgBasket" src="img\\${goods[id].img_goods}">`;
+            out += ` ${goods[id].name  }`;
+            out += ` ${cart[id]  }`;
+            out += `<button data-id="${id}" class="del-goods"><img src="img/trash.png"></button>`;
+            out += '</div>';
+        }
+        $('.cart').html(out);
+    })
+};
+
 
 
 
 $(document).ready(function () {
     loadCart();
+    showCart();
 
 });
