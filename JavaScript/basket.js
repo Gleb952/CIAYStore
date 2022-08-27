d42cb8e59b076c2417ae8bba54e759dd18cd147d
Gleb952 <lomakin.plex@mail.ru>
2022-08-27 13:42:52 +0300

    showCart_posters();     showCart_Stickers();

--- JavaScript/basket.js
+++ JavaScript/basket.js
@@ -3,7 +3,8 @@ var cart = {};
 function loadCart() {
     if (localStorage.getItem('cart')) {
         cart = JSON.parse(localStorage.getItem('cart'));
-        showCart();
+        showCart_posters();
+        showCart_Stickers();
     } else {
         $(".cart").html("None");
     }
@@ -12,7 +13,7 @@ function loadCart() {
 
 
 
-function showCart() {
+function showCart_posters() {
     $.getJSON("posters.json", function (data) {
         var goods = data;
         var out = '';
@@ -28,13 +29,32 @@ function showCart() {
         $('.del-goods').on('click', delGoods);
     })
 };
+function showCart_Stickers() {
+    $.getJSON("Stickers.json", function (data) {
+        var goods = data;
+        var out = '';
+        for (var id in cart) {
+            out += `<div class="cartBasket">`;
+            out += `<img class="imgBasket" src="img\\${goods[id].img_goods}">`;
+            out += ` ${goods[id].name  }`;
+            out += ` ${cart[id]  }`;
+            out += `<button data-id="${id}" class="del-goods"><img src="img/trash.png"></button>`;
+            out += '</div>';
+        }
+        $('.cart').html(out);
+        $('.del-goods').on('click', delGoods);
+    })
+};
+
+
 
 function delGoods() {
     //удаляем товар из корзины
     var id = $(this).attr('data-id');
     delete cart[id];
     saveCart();
-    showCart();
+    showCart_posters();
+    showCart_Stickers();
 }
 
 function saveCart() {
