4000f10e377c275d8a1a39f37f00778063874b75
Gleb952 <lomakin.plex@mail.ru>
2022-09-05 20:24:10 +0300

    hoodie.json img basket html

--- JavaScript/basket.js
+++ JavaScript/basket.js
@@ -1,33 +1,37 @@
 var cart = {};
 
 function loadCart() {
+    //проверяю есть ли в localStorage запись cart
     if (localStorage.getItem('cart')) {
+        // если есть - расширфровываю и записываю в переменную cart
         cart = JSON.parse(localStorage.getItem('cart'));
         showCart();
     } else {
-        $(".cart").html("None");
+        $('.mainCart').html('Корзина пуста!');
     }
 }
 
-
-
-
-function showCart(file) {
-    $.getJSON(file, function (data) {
-        var goods = data;
-        var out = '';
-        for (var id in cart) {
-            out += `<div class="cartBasket">`;
-            out += `<img class="imgBasket" src="img\\${goods[id].img_goods}">`;
-            out += ` ${goods[id].name  }`;
-            out += ` ${cart[id]  }`;
-            out += `<button data-id="${id}" class="del-goods"><img src="img/trash.png"></button>`;
-            out += '</div>';
-        }
-        $('.cart').html(out);
-        $('.del-goods').on('click', delGoods);
-    })
-};
+function showCart() {
+    //вывод корзины
+    if (!isEmpty(cart)) {
+        $('.mainCart').html('Корзина пуста!');
+    } else {
+        $.getJSON('hoodie.json', function (data) {
+            var goods = data;
+            var out = '';
+            for (var id in cart) {
+                out += `<div class="cartBasket">`;
+                out += `<img class="imgBasket" src="img\\${goods[id].img_goods}">`;
+                out += ` ${goods[id].name  }`;
+                out += ` ${cart[id]  }`;
+                out += `<button data-id="${id}" class="del-goods"><img src="img/trash.png"></button>`;
+                out += '</div>';
+            }
+            $('.mainCart').html(out);
+            $('.del-goods').on('click', delGoods);
+        });
+    }
+}
 
 function delGoods() {
     //удаляем товар из корзины
@@ -40,6 +44,13 @@ function delGoods() {
 function saveCart() {
     //сохраняю корзину в localStorage
     localStorage.setItem('cart', JSON.stringify(cart)); //корзину в строку
+}
+
+function isEmpty(object) {
+    //проверка корзины на пустоту
+    for (var key in object)
+        if (object.hasOwnProperty(key)) return true;
+    return false;
 }
 
 
